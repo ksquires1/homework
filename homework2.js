39cb37c0dba245e5b00e906392f499334a2f772e
Keenan <keenansquires@gmail.com>
2021-07-05 22:17:23 -0500

    Did the homework

---
+++ homework2.js
@@ -0,0 +1,12 @@
+let pizzaPlace = "pizzahut";
+let numberOfToppings = 3; 
+
+console.log(`hello welcome to ${pizzaPlace} ${typeof pizzaPlace} we had ${numberOfToppings} ${typeof numberOfToppings} toppings`);
+
+if (numberOfToppings < 10) {
+    console.log("quality not quantity"); 
+    }
+        else
+    {
+    console.log("a whole lot of pizza");
+}
